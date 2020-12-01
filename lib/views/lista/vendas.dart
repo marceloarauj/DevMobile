4e9e90962ec69fc4b1237c59138af028e82e243c
Marcelo Araújo <tata-marcelo@hotmail.com>
2020-11-30 23:00:17 -0300

    Finalizando a venda

--- lib/views/lista/vendas.dart
+++ lib/views/lista/vendas.dart
@@ -18,7 +18,7 @@ class ItensLista {
           lista:lista,
           uid: uid,
           id: venda.venda_id,
-          Tipo: DeParaTipo(venda.status_venda),
+          tipo: DeParaTipo(venda.status_venda),
           movel: DeParaMovel(venda.movel),
           data: FormatedData(venda.data_venda)));
     }
@@ -74,19 +74,23 @@ class ItensLista {
   }
 }
 
-class ElementoLista extends StatelessWidget {
+class ElementoLista extends StatefulWidget {
   
-  ElementoLista({Key key, this.Tipo, this.data, this.movel, this.id, this.uid,this.lista})
+  ElementoLista({Key key, this.tipo, this.data, this.movel, this.id, this.uid,this.lista})
       : super(key: key);
   
   VendaServices services = VendaServices();
-  final Tipo;
+  Tipo tipo;
   final String data;
   final String movel;
   final int id;
   final int uid;
   final ListaViewUser lista;
 
+  @override
+  _ElementoLista createState()=> _ElementoLista();
+}
+class _ElementoLista extends State<ElementoLista>{
   String DeParaMovel(int movel) {
     if (movel == 1) {
       return 'Cama';
@@ -140,7 +144,7 @@ class ElementoLista extends StatelessWidget {
                         );
                       });
                     }),
-                venda = await services.comprarMovel(idVenda, uid),
+                venda = await widget.services.comprarMovel(idVenda, uid),
                 Navigator.pop(dialogContx),
                 showDialog(
                     context: context,
@@ -157,9 +161,8 @@ class ElementoLista extends StatelessWidget {
                         );
                       });
                     }),
-                sleep(Duration(seconds:2)),
-                lista.setState(()=>{}),
-                                
+                widget.tipo = Tipo.Comprado,
+                setState(()=>{})               
               },
           elevation: 3.5,
           color: Colors.green,
@@ -179,7 +182,7 @@ class ElementoLista extends StatelessWidget {
     return Material(
       child: InkWell(
           onTap: () async {
-            List<VendaDTO> venda = await services.obterVendaPorId(id);
+            List<VendaDTO> venda = await widget.services.obterVendaPorId(widget.id);
             showDialog(
                 context: context,
                 barrierDismissible: false,
@@ -201,7 +204,7 @@ class ElementoLista extends StatelessWidget {
                               child: fromBase64Image(venda[0].imagem),
                             ),
                             podeComprar(venda[0].status_venda,
-                                id, uid, context)
+                                widget.id, widget.uid, context)
                           ],
                         )),
                       ),
@@ -220,25 +223,30 @@ class ElementoLista extends StatelessWidget {
                 children: <Widget>[
                   Padding(
                     padding: EdgeInsets.only(top: 10, left: 10),
-                    child: Text("${movel}"),
+                    child: Text("${widget.movel}"),
                   ),
                   Padding(
                     padding: EdgeInsets.only(top: 10, left: 0),
-                    child: Text("${data}"),
+                    child: Text("${widget.data}"),
                   ),
                   Padding(
                     padding: EdgeInsets.only(top: 10, right: 10),
-                    child: Situacao(tipo: Tipo),
+                    child: Situacao(tipo: widget.tipo),
                   ),
                 ],
               ))),
     );
   }
-}
 
-class Situacao extends StatelessWidget {
+}
+class Situacao extends StatefulWidget {
   const Situacao({Key key, this.tipo}) : super(key: key);
   final Tipo tipo;
+
+  @override
+  _Situacao createState()=> _Situacao();
+}
+class _Situacao extends State<Situacao>{
 
   @override
   Widget build(BuildContext context) {
@@ -252,13 +260,13 @@ class Situacao extends StatelessWidget {
   }
 
   Color cor() {
-    if (tipo == Tipo.Disponivel) {
+    if (widget.tipo == Tipo.Disponivel) {
       return Colors.green;
     }
-    if (tipo == Tipo.Comprado) {
+    if (widget.tipo == Tipo.Comprado) {
       return Colors.brown;
     }
-    if (tipo == Tipo.Entregue) {
+    if (widget.tipo == Tipo.Entregue) {
       return Colors.blue;
     }
 
@@ -266,17 +274,17 @@ class Situacao extends StatelessWidget {
   }
 
   Text texto() {
-    if (tipo == Tipo.Disponivel) {
+    if (widget.tipo == Tipo.Disponivel) {
       return Text("Disponível", style: TextStyle(color: Colors.white));
     }
-    if (tipo == Tipo.Comprado) {
+    if (widget.tipo == Tipo.Comprado) {
       return Text("Comprado", style: TextStyle(color: Colors.white));
     }
-    if (tipo == Tipo.Entregue) {
+    if (widget.tipo == Tipo.Entregue) {
       return Text("Entregue", style: TextStyle(color: Colors.white));
     }
     return Text("");
   }
-}
 
+}
 enum Tipo { Disponivel, Comprado, Entregue }
